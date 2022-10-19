5573d70b979e7149ce8e5fd0285c9e46b44e5b0f
Przemyslaw Pajak <office@fearlessspider.com>
2022-10-19 18:43:36 -0400

    Initial config

--- mix.exs
+++ mix.exs
@@ -22,8 +22,11 @@ defmodule Spiderslab.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ecto_sql, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:plug_cowboy, "~> 2.0"},
+      {:absinthe, "~> 1.7"},
+      {:absinthe_plug, "~> 1.5.8"}
     ]
   end
 end
