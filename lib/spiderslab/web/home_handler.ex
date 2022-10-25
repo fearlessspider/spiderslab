9e6d7dd858e5a7651d4b65a657d5887eb7a26830
Przemyslaw Pajak <office@fearlessspider.com>
2022-10-24 21:08:47 -0400

    Added simple template render on the fly

--- lib/spiderslab/web/home_handler.ex
+++ lib/spiderslab/web/home_handler.ex
@@ -1,4 +1,8 @@
 defmodule Spiderslab.Web.HomeHandler do
+  import Spiderslab.Utils.Conn
+
+  @template_path "lib/spiderslab/web/templates"
+
   def init(req, state) do
     resp =
       :cowboy_req.reply(
@@ -13,5 +17,16 @@ defmodule Spiderslab.Web.HomeHandler do
 
   def terminate(_reason, _req, _state) do
     :ok
+  end
+
+  def render(conn, name) do
+    conn = assign(conn, :adj, "great")
+    body = EEx.eval_file("#{@template_path}/#{name}.eex", Enum.to_list(conn.assign))
+    put_resp_body(conn, body)
+  end
+
+  defp get_templates() do
+    for t <- Path.wildcard("#{@template_path}/*.eex"),
+        do: Path.basename(t, ".eex")
   end
 end
